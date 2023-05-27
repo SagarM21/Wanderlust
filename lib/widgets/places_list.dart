3b77bdd0268283ca61a79eb34c57d13d93b7b1cf
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-05-27 14:50:07 +0530

    Add: Place Details Screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 class PlacesList extends StatelessWidget {
@@ -25,6 +26,13 @@ class PlacesList extends StatelessWidget {
                 .textTheme
                 .titleMedium!
                 .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
